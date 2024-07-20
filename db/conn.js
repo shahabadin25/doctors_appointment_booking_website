4674a8756f4a30ff428d61f0cf0c49cf27f4b9da
Adin Shahab <shahabadin25@outlook.com>
2024-07-20 16:19:18 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose= require("mongoose");
 
-mongoose.connect("mongodb+srv://shahabadin25:Cx4Z5WQoYLmvyto1@cluster0.vi2nyjz.mongodb.net/appointment",{
+mongoose.connect("mongodb+srv://shahabadin25:adin2001@cluster0.hcla0xa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/appointment",{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
